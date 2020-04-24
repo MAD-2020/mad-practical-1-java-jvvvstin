f0631122f1a1d6a1d47262ae2fd35dec66e84642
jvvvstin <63656476+jvvvstin@users.noreply.github.com>
2020-04-24 12:30:37 +0800

    Update Question5.java

--- Question5.java
+++ Question5.java
@@ -2,6 +2,26 @@ import java.util.Scanner;
 
 public class Question5
 {
+  public static int getMode(int[] numArray) {
+    int count = 0;
+    int tempCount;
+    int tempMode;
+    int actualMode = numArray[0];
+    for (int i : numArray) {
+      tempMode = i;
+      tempCount = 0;
+      for (int n : numArray) {
+        if (tempMode == n) {
+          tempCount++;
+        }
+      }
+      if (tempCount > count) {
+        count = tempCount;
+        actualMode = tempMode;
+      }
+    }
+    return actualMode;
+  }
   public static void main(String[] args)
   {
     /**
@@ -27,6 +47,14 @@ public class Question5
      */
      
     Scanner in = new Scanner(System.in);
+    //System.out.print("Enter in number of inputs: ");
+    int noOfInputs = in.nextInt();
+    int[] intArray = new int[noOfInputs];
+    for (int i = 0; i < noOfInputs; i++) {
+      //System.out.printf("Enter a number (%d): ", i+1);
+      intArray[i] = in.nextInt();
+    }
+    System.out.println("The mode is " + getMode(intArray));
     
   }
 }
